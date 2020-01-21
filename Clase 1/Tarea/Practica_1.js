7a7088faa4ea72e7f60660a8099c2b371d6649d0
femavana <femavana@gmail.com>
2020-01-20 22:43:35 -0300

    Update and rename tarea-clase-1.js to Practica_1.js

--- Clase 1/Tarea/Practica_1.js
+++ Clase 1/Tarea/Practica_1.js
@@ -1,13 +1,14 @@
+
 // crear una función que tome como parámetro el año actual y el año de nacimiento
 // y calcule la edad del usuario (más o menos).
 // Preguntarle estos datos al usuario y guardarlos en 2 variables
 // Ejecutar la función con estos datos
 // Impriman el resultado en la consola
 
-
 // Preguntar el salario anual y calcular el salario mensual
 // Preguntar el salario mensual y calcular el anual
 // diario... semanal, por hora. etc.
+
 function calcularEdad(anioActual, anioNacimiento) {
     let edadUsuario=anioActual - anioNacimiento;
     return edadUsuario;
@@ -65,13 +66,12 @@ console.log('Tu salario diario es ' + calcularSalarioDiario(salarioAnual));
 
 // function hoisting
 
-/*
 pruebaHoisting(); //funciona!
 function pruebaHoisting(){
     console.log('prueba');
-}
-//pruebaHoisting();
-*/
+
+ruebaHoisting();
+
 
 /*
 var a = 1; //global
